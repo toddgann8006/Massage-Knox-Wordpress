c0837c3ff0fc76152c10470a084c1682fc766c85
toddgann8006 <89031976+toddgann8006@users.noreply.github.com>
2022-05-08 21:29:58 -0400

    deploy update

--- page.php
+++ page.php
@@ -4,43 +4,18 @@ get_header();
 
 while(have_posts()) {
     the_post(); ?>
-
-    <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/knoxville2.jpg') ?>)"></div>
-      <div class="page-banner__content container t-center c-white">
-        <h1 class="headline headline--medium"><?php the_title(); ?></h1>
-      </div>
-    </div>
-    <?php if (is_page('About')) {?>
-    <div class="about-container">
-  <div class="about-content-container">
-    <?php echo get_the_content(); ?>
-  </div>  
-  <div class="about-content-container">
-  <img src="<? echo get_theme_file_uri('/images/profile1.jpg') ?>" class="main-profile" />
-  </div> 
+<div style="background-image: url(<?php echo get_theme_file_uri('/images/knoxville.jpg') ?>)" class="page-image-container">
+<div class="page-image-header">
+  <h1><?php the_title(); ?></h1>
+</div>  
 </div>
-  
-<?php
-    } if (is_page('Reviews')){ ?>
-      <div class="reviews">
-    <?php  echo do_shortcode( '[grw id="142"]' );?>
-    </div>
-   <?php } if(!is_page('About') && !is_page('Reviews')) {
-        ?>
-        <div class="services-content-container">
+<div class="services-content-container">
     <?php echo get_the_content(); ?>
-    </div>
-    <?php
-    }
-    ?>
-
+</div>
 <?php
 }
 ?>
-
 <hr>
-
 <?php
 
 get_footer();
